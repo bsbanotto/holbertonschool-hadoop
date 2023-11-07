ecd94b25ece25ee93d0ce238727f244ae7198f9b
bsbanotto <ben.sbanotto@gmail.com>
2023-11-07 12:02:48 -0600

    Commit for task 4. Fun fact, mkdir returns a generator that must be consumed for it to execute. That is why we print the created directories!

---
+++ hadoop_project/4-createdir.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+"""
+Utilize Snakebite to create all directories in a list
+"""
+from snakebite.client import Client
+
+
+def createdir(l):
+    """
+    Args:
+        l: list of directories to create withing HDFS
+    """
+    client = Client('localhost', 9000)
+
+    for created_dir in client.mkdir(l, create_parent=True):
+        print(created_dir)
